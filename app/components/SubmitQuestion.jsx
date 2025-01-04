add7d3930c95f45723c9084512937fd088552763
DynamicDOMinator <ahmedsayedhamed@gmail.com>
2025-01-04 20:36:55 +0200

    updated

--- app/components/SubmitQuestion.jsx
+++ app/components/SubmitQuestion.jsx
@@ -102,6 +102,7 @@ export default function SubmitQuestion() {
             name="phone"
             value={formData.phone}
             onChange={handleChange}
+            maxLength={9}
             className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
           />
         </div>
